107244a51d83d5a9a1e1f59799f7d27e665ebc15
so10000 <296763318+so10000@users.noreply.github.com>
2026-07-13 09:48:19 +0900

    Refactor mouseover event logic for elements
    Refactored mouseover event handling for '.butterfly' and '.foo' elements to improve boundary checking and movement logic.

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 document.querySelectorAll('.butterfly');
 document.querySelectorAll('.butterfly').forEach(function (el) {
     el.addEventListener('mouseover', function () {
-        t = Math.floor(Math.random() * (474 - 350 + 1)) + 350;
-        l = Math.floor(Math.random() * (880));
-        el.style.left = l + 'px';
-        el.style.top  = t + 'px';
+        bt_l = el.offsetLeft;
+        bt_t = el.offsetTop;
+        // t = Math.floor(Math.random() * (474 - 350 + 1)) + 350;
+        // l = Math.floor(Math.random() * (880));
+        // el.style.left = l + 'px';
+        // el.style.top  = t + 'px';
+        t = Math.floor(Math.random() * 248) - 124;
+        l = Math.floor(Math.random() * 880) - 440;
+        
+        if ((bt_l + l >= 0 && bt_l + l <= 880) && (bt_t + t >= 350 && bt_t + t <= 474)){
+            el.style.left = bt_l + l + 'px';
+            el.style.top  = bt_t + t + 'px';
+        } else if (bt_l + l >= 0 && bt_l + l <= 880){
+            if (bt_t + t < 350){
+                el.style.left = bt_l + l + 'px';
+                el.style.top  = 350 + (350 - bt_t - t) + 'px';
+            } else{
+                el.style.left = bt_l + l + 'px';
+                el.style.top  = 474 - (bt_t + t - 474) + 'px';
+            }
+        } else if (bt_t + t >= 350 && bt_t + t <= 474){
+            if (bt_l + l < 0){
+                el.style.left = 0 + (0 - bt_l - l) + 'px';
+                el.style.top  = bt_t + t + 'px';
+            } else{
+                el.style.left = 880 - (bt_l + l - 880) + 'px';
+                el.style.top  = bt_t + t + 'px';
+            }
+        }
     });
 });
 document.querySelectorAll('.foo');
@@ -15,15 +40,27 @@ document.querySelectorAll('.foo').forEach(function (el) {
         foo_t = parseInt(style.top, 10);
         
         t = Math.floor(Math.random() * 220) - 110;
-        l = Math.floor(Math.random() * 220) - 110;
+        l = Math.floor(Math.random() * 450) - 225;
         
         if ((foo_l + l >= 365 && foo_l + l <= 815) && (foo_t + t >= 190 && foo_t + t <= 300)){
             el.style.left = foo_l + l + 'px';
             el.style.top  = foo_t + t + 'px';
         } else if (foo_l + l >= 365 && foo_l + l <= 815){
-            el.style.left = foo_l + l + 'px';
+            if (foo_t + t < 190){
+                el.style.left = foo_l + l + 'px';
+                el.style.top  = 190 + (190 - foo_t - t) + 'px';
+            } else{
+                el.style.left = foo_l + l + 'px';
+                el.style.top  = 300 - (foo_t + t - 300) + 'px';
+            }
         } else if (foo_t + t >= 190 && foo_t + t <= 300){
-            el.style.top  = foo_t + t + 'px';
+            if (foo_l + l < 365){
+                el.style.left = 365 + (365 - foo_l - l) + 'px';
+                el.style.top  = foo_t + t + 'px';
+            } else{
+                el.style.left = 815 - (foo_l + l - 815) + 'px';
+                el.style.top  = foo_t + t + 'px';
+            }
         }
     });
 });
